829e7c1959a36b1d7415db6c40aee237ef59be65
Puhnatiy <55509995+Puhnatiy@users.noreply.github.com>
2020-08-01 07:29:30 +0300

    Update tests_test_game.py

--- tests/tests_test_game.py
+++ tests/tests_test_game.py
@@ -2,68 +2,9 @@ import pytest
 from enum import Enum
 import time
 import random
-#from app import choose_word, WORDS_LIST, \
-#calculate_score, create_game, MAX_AMOUNT_OF_ATTEMPS, Result, next_step
-WORDS_LIST = ['skillfactory', 'testing', 'blackbox', 'pytest', 'unittest', 'coverage']
-MAX_AMOUNT_OF_ATTEMPS = 4
-def next_step(result):
-    if result == Result.WIN:
-        print("You win")
-    elif result == Result.FAIL:
-        print("You lost")
-    else:
-        time.sleep(0.1)
-        return True
-class Result(Enum):
-    FAIL = 0
-    WIN = 1
-    CONTINUE = -1
-class Game():
-    def __init__(self):
-        self.answer = choose_word(WORDS_LIST)
-        self.guess_count = 0
-        self.guessed_letters = []
-
-    def guess(self, letter):
-        if not letter.isalpha():
-            raise ValueError
-        if self.get_result() != Result.CONTINUE:
-            raise ValueError
-        self.guessed_letters.append(letter)
-        if letter.lower() in self.answer:
-            return True
-        else:
-            self.guess_count += 1
-            return False
-
-    def get_current_state(self):
-        current_state = []
-        for i in self.answer:
-            if i in self.guessed_letters:
-                current_state.append(i)
-            else:
-                current_state.append('_')
-        return ''.join(current_state)
-
-    def get_result(self):
-        if self.guess_count >= MAX_AMOUNT_OF_ATTEMPS:
-            return Result.FAIL
-        elif self.answer == self.get_current_state():
-            return Result.WIN
-        else:
-            return Result.CONTINUE
-
-def create_game():
-    game = Game()
-    return game
-
-def choose_word(word_list):
-    return random.choice(word_list)
-def calculate_score(wrong_guesses):
-    return max(MAX_AMOUNT_OF_ATTEMPS - wrong_guesses, 0)
-    
-    
-
+from ..app import choose_word, WORDS_LIST, \
+calculate_score, create_game, MAX_AMOUNT_OF_ATTEMPS, Result, next_step
+ 
 def test_choose_word():
     random_word = choose_word(WORDS_LIST)
     assert random_word in WORDS_LIST
